a106506a56aeb8a65a389864c379587ad322e728
AngrySigma <illariov1809@gmail.com>
2023-12-05 05:08:26 +0300

    Continue experiment

--- src/optimization/run_experiment.py
+++ src/optimization/run_experiment.py
@@ -1,17 +1,24 @@
 import hydra
 from omegaconf import DictConfig
-from src.optimization.data_operations.dataset_loaders import load_dataset
+from src.optimization.data_operations.dataset_loaders import DatasetLoader
 from src.optimization.llm.llm_templates import LLMTemplate
+from src.optimization.llm.gpt import ChatBot, ChatMessage
+
 
 @hydra.main(version_base=None,
-            config_path='D:/PhD/LAAFE/src/optimization',
+            config_path='D:/PhD/LAAFE/',
             config_name='cfg')
 def main(cfg: DictConfig):
     dataset_id = cfg[cfg.problem_type].datasets[0]
-    dataset = load_dataset(dataset_id)
+    dataset_loader = DatasetLoader(dataset_ids=[dataset_id])
+    dataset = dataset_loader[0]
     llm_template = LLMTemplate(operators=cfg.llm.operators)
-    print(dataset)
-    print(llm_template)
+    chatbot = ChatBot(api_key=cfg.llm.gpt.openai_api_key,
+                      api_organization=cfg.llm.gpt.openai_api_organization,
+                      model=cfg.llm.gpt.model_name)
+    print(chatbot.get_completion(messages=[ChatMessage(role='user', content='hello')]))
+    # print(llm_template)
+    # print(dataset)
 
 
 # define data operation pipeline format (fix)
