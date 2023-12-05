ac62296f32442f275aed10cda2c6d6674672222b
AngrySigma <illariov1809@gmail.com>
2023-12-05 05:08:07 +0300

    Fit to new API version

--- src/optimization/llm/gpt.py
+++ src/optimization/llm/gpt.py
@@ -1,8 +1,7 @@
 import os
 from dataclasses import dataclass
 
-import openai
-from dotenv import load_dotenv
+from openai import OpenAI
 
 
 @dataclass
@@ -15,24 +14,22 @@ class ChatMessage:
 
 
 class ChatBot:
-    def __init__(self, model="gpt-3.5-turbo"):
-        load_dotenv()
-        self.api_key = os.getenv("OPENAI_API_KEY")
-        self.organization = os.getenv("OPENAI_API_ORGANIZATION")
-        openai.api_key = self.api_key
-        openai.organization = self.organization
+    def __init__(self, api_key, api_organization, model="gpt-3.5-turbo"):
+        self.client = OpenAI(api_key=api_key, organization=api_organization)
         self.model = model
 
     def get_completion(
-        self, messages: ChatMessage, max_tokens=150, stop=None, temperature=1, n=1
+            self, messages: ChatMessage, max_tokens=150, stop=None,
+            temperature=1, n=1
     ):
         """
         Get completion from openai chatbot
         """
         messages = [
-            {"role": message.role, "content": message.content} for message in messages
+            {"role": message.role, "content": message.content} for message in
+            messages
         ]
-        response = openai.ChatCompletion.create(
+        response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
             max_tokens=max_tokens,
@@ -40,3 +37,4 @@ class ChatBot:
             stop=stop,
             temperature=temperature,
         )
+        return response.choices[0].message.content
