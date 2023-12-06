b9e8a212e727099cdf194b724b638996fc4d69c3
AngrySigma <illariov1809@gmail.com>
2023-12-06 14:17:47 +0300

    Add message history

--- src/optimization/llm/gpt.py
+++ src/optimization/llm/gpt.py
@@ -6,12 +6,38 @@ from openai import OpenAI
 
 @dataclass
 class ChatMessage:
-    role: str
     content: str
+    role: str = "user"
 
     def __str__(self):
         return f"{self.role}: {self.content}"
 
+
+class MessageHistory:
+    def __init__(self, messages: list[ChatMessage] | None = None):
+        self.messages = messages if messages is not None else []
+
+    def add_message(self, content: str, role: str = "user"):
+        self.messages.append(ChatMessage(role=role, content=content))
+
+    def add_pipeline_evaluation(self, pipeline: str, metrics: float):
+        self[-2].content += f"\nPipeline: {pipeline}, Metrics: {metrics}"
+
+
+    def __str__(self):
+        return "\n".join([str(message) for message in self.messages])
+
+    def __add__(self, other):
+        return MessageHistory(self.messages + other.messages)
+
+    def __len__(self):
+        return len(self.messages)
+
+    def __getitem__(self, item):
+        return self.messages[item]
+
+    def __iter__(self):
+        return iter(self.messages)
 
 class ChatBot:
     def __init__(self, api_key, api_organization, model="gpt-3.5-turbo"):
@@ -19,7 +45,7 @@ class ChatBot:
         self.model = model
 
     def get_completion(
-            self, messages: ChatMessage, max_tokens=150, stop=None,
+            self, messages: list[ChatMessage], max_tokens=150, stop=None,
             temperature=1, n=1
     ):
         """
