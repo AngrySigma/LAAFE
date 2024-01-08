a28f3c0454112087084e05b0d2ed550830bd9288
AngrySigma <illariov1809@gmail.com>
2024-01-08 22:06:53 +0300

    Move mappings to init

---
+++ src/optimization/__init__.py
@@ -0,0 +1,10 @@
+from src.optimization.models.model_training import (
+    CatboostClassifierModel, LinearClassifierModel,
+    RandomForestClassifierModel, SVMClassifierModel)
+
+MODELS = {
+    "catboost_classifier": CatboostClassifierModel,
+    "linear_classifier": LinearClassifierModel,
+    "svm_classifier": SVMClassifierModel,
+    "random_forest_classifier": RandomForestClassifierModel,
+}
---
+++ src/optimization/data_operations/__init__.py
@@ -0,0 +1,20 @@
+from src.optimization.data_operations.operation_aliases import (
+    Add, Binning, Div, Drop, FillnaMean, FillnaMedian, FrequencyEncoding,
+    LabelEncoding, Minmax, Mul, OneHotEncoding, Pca, Std, Sub)
+
+OPERATIONS = {
+    "add": Add,
+    "sub": Sub,
+    "mul": Mul,
+    "div": Div,
+    "pca": Pca,
+    "fillnamean": FillnaMean,
+    "fillnamedian": FillnaMedian,
+    "std": Std,
+    "minmax": Minmax,
+    "drop": Drop,
+    "frequencyencoding": FrequencyEncoding,
+    "binning": Binning,
+    "labelencoding": LabelEncoding,
+    "onehotencoding": OneHotEncoding,
+}
