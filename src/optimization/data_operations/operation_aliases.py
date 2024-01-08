699adc51622cbf7b43bf9451d8c8e4e8e3dc5b16
AngrySigma <illariov1809@gmail.com>
2024-01-08 22:09:40 +0300

    Create operation pipeline to transform both the train and test data

--- src/optimization/data_operations/operation_aliases.py
+++ src/optimization/data_operations/operation_aliases.py
@@ -1,50 +1,75 @@
-import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+
 
 class Operation(ABC):
-    def __init__(self, description=None, *args, **kwargs):
-        self.description = description if description is not None else ""
+    def __init__(self, inp=None, *args, **kwargs):
+        self.inp = [inp] if isinstance(inp, str) else inp
 
     @abstractmethod
-    def __call__(self, df, inp):
+    def __call__(self, df):
+        pass
+
+    def fit_transform(self, df):
+        self.inp = df.columns if self.inp is None else self.inp
+
+    @abstractmethod
+    def transform(self, df):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def description(cls):
         pass
 
     def __str__(self):
-        return self.description
+        return self.description()
 
     def __repr__(self):
-        return self.description
+        return self.description()
 
 
+# TODO: check this. Unnecessary, can be deleted
 class InplaceOperation(Operation, ABC):
-    def __init__(self, description=None, *args, **kwargs):
-        super().__init__()
+    def __init__(self, inp=None, *args, **kwargs):
+        super().__init__(inp, *args, **kwargs)
 
     @abstractmethod
-    def __call__(self, df, inp):
+    def __call__(self, df) -> None:
+        pass
+
+
+# TODO: update this
+class ReturnOperation(Operation, ABC):
+    def __init__(self, inp=None, *args, **kwargs):
+        super().__init__(inp, *args, **kwargs)
+
+    @abstractmethod
+    def __call__(self, df) -> pd.DataFrame:
         return df
 
 
-class ReturnOperation(Operation, ABC):
-    def __init__(self, description=None, *args, **kwargs):
-        super().__init__()
-
-    @abstractmethod
-    def __call__(self, df, inp):
-        return df[inp]
-
-
 class Drop(InplaceOperation):
-    def __init__(self):
-        super().__init__()
-        self.description = "Drop input columns inplace"
+    def __init__(self, inp=None):
+        super().__init__(inp)
 
-    def __call__(self, df, inp):
-        df.drop(columns=inp, inplace=True)
+    @classmethod
+    def description(cls):
+        return "Drop input columns inplace"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def transform(self, df):
+        df.drop(columns=self.inp, inplace=True)
+        return df
+
+    def fit_transform(self, df):
+        self.transform(df)
         return df
 
 
@@ -88,87 +113,246 @@ class Div(Operation):
         return df[inp[0]].div(df[inp[1]])
 
 
+# TODO: add train and test transformations
 class Pca(InplaceOperation):
-    def __init__(self):
-        super().__init__()
-        self.pca = None
-        self.description = "Create new column from PCA on input columns"
+    def __init__(self, inp=None):
+        super().__init__(inp)
+        self.pca = PCA(0.95)
 
-    def __call__(self, df, inp):
-        inp = inp if inp else df.columns
-        # if self.pca is None:
-        self.pca = PCA(1)
-        df["pca"] = self.pca.fit_transform(df[inp])
-        # df.drop(columns=inp, inplace=True)
+    @classmethod
+    def description(cls):
+        return "Create new column from PCA on input columns"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        # TODO: fill na if there are any. moreover, this should be added to pipeline
+        self.inp = df.columns if self.inp is None else self.inp
+        pca_result = self.pca.fit_transform(df[self.inp])
+        pca_columns = [f"pca_{i}" for i in range(pca_result.shape[1])]
+        df[pca_columns] = pca_result
         return df
-        # df['pca'] = self.pca.transform(df[inp])
-        # df.drop(columns=inp, inplace=True)
-        # return df
-        # TODO: fix pca initialization. now it uses the same pca if not initialized anew each time
+
+    def transform(self, df):
+        pca_result = self.pca.transform(df[self.inp])
+        pca_columns = [f"pca_{i}" for i in range(pca_result.shape[1])]
+        df[pca_columns] = pca_result
+        return df
 
 
 class FillnaMean(InplaceOperation):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, inp=None):
+        super().__init__(inp)
         self.mean = None
-        self.description = "Fill missing values with mean inplace"
+
+    @classmethod
+    def description(cls):
+        return "Fill missing values with mean inplace"
 
     def __call__(self, df, inp=None):
-        inp = inp if inp else df.columns
-        if self.mean is None:
-            self.mean = df[inp].mean()
-        df[inp] = df[inp].fillna(self.mean)
+        return self.fit_transform(df, inp)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        self.inp = df.columns if self.inp is None else self.inp
+        # TODO: [col for col in inp if col in df.columns]
+        self.mean = df[self.inp].mean()
+        df[self.inp] = df[self.inp].fillna(self.mean)
+        return df
+
+    def transform(self, df):
+        df[self.inp] = df[self.inp].fillna(self.mean)
         return df
 
 
 class FillnaMedian(InplaceOperation):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, inp=None):
+        super().__init__(inp)
         self.median = None
-        self.description = "Fill missing values with median inplace"
+
+    @classmethod
+    def description(cls):
+        return "Fill missing values with median inplace"
 
     def __call__(self, df, inp=None):
-        inp = inp if inp else df.columns
-        if self.median is None:
-            self.median = df[inp].median()
-        df[inp] = df[inp].fillna(self.median)
+        return self.fit_transform(df, inp)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        self.median = df[self.inp].median()
+        df[self.inp] = df[self.inp].fillna(self.median)
+        return df
+
+    def transform(self, df):
+        df[self.inp] = df[self.inp].fillna(self.median)
         return df
 
 
 class Std(InplaceOperation):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, inp=None):
+        super().__init__(inp)
         self.scaler = StandardScaler()
-        self.description = "Inplace standard scaling of input columns"
+
+    @classmethod
+    def description(cls):
+        return "Inplace standard scaling of input columns"
 
     def __call__(self, df, inp=None):
-        inp = inp if inp else df.columns
-        df[inp] = self.scaler.fit_transform(df[inp])
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        df[self.inp] = self.scaler.fit_transform(df[self.inp])
+        return df
+
+    def transform(self, df):
+        df[self.inp] = self.scaler.transform(df[self.inp])
         return df
 
 
+# TODO: here we can create scaler subclass
 class Minmax(InplaceOperation):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, inp=None):
+        super().__init__(inp)
         self.scaler = MinMaxScaler()
-        self.description = "Inplace minmax scaling of input columns"
+
+    @classmethod
+    def description(cls):
+        return "Inplace minmax scaling of input columns"
 
     def __call__(self, df, inp=None):
-        inp = inp if inp else df.columns
-        df[inp] = self.scaler.fit_transform(df[inp])
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        df[self.inp] = self.scaler.fit_transform(df[self.inp])
+        return df
+
+    def transform(self, df):
+        df[self.inp] = self.scaler.transform(df[self.inp])
+        return df
+
+
+class FrequencyEncoding(InplaceOperation):
+    def __init__(self, inp=None):
+        super().__init__(inp)
+
+    @classmethod
+    def description(cls):
+        return "Frequency encoding of categorical features"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        for col in self.inp:
+            df[col] = df.groupby(col)[col].transform("count")
+        return df
+
+    def transform(self, df):
+        # TODO: here, for shorter test set, we will get something else.
+        #  also, if there are matching entities, no info will be captured.
+        #  need to be fixed
+        for col in self.inp:
+            df[col] = df.groupby(col)[col].transform("count")
+        return df
+
+
+class Binning(InplaceOperation):
+    def __init__(self, inp=None, bins=10):
+        super().__init__(inp)
+        self.bins = bins
+
+    @classmethod
+    def description(cls):
+        return "Binning of numerical features"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        for col in self.inp:
+            df[col] = pd.qcut(df[col], 10, duplicates="drop")
+        return df
+
+    def transform(self, df):
+        # TODO: here, for test set, binning will be different. need to be fixed
+        for col in self.inp:
+            df[col] = pd.qcut(df[col], 10, duplicates="drop")
+        return df
+
+
+class LabelEncoding(InplaceOperation):
+    def __init__(self, inp=None):
+        super().__init__(inp)
+        self.label_encoders = []
+
+    @classmethod
+    def description(cls):
+        return "Label encoding of categorical features"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        for col in self.inp:
+            encoder = LabelEncoder()
+            df[col] = encoder.fit_transform(df[col])
+            self.label_encoders.append(encoder)
+        return df
+
+    def transform(self, df):
+        for col, encoder in zip(self.inp, self.label_encoders):
+            df[col] = encoder.transform(df[col])
+        return df
+
+
+class OneHotEncoding(InplaceOperation):
+    def __init__(self, inp=None):
+        super().__init__(inp)
+
+    @classmethod
+    def description(cls):
+        return "One hot encoding of categorical features"
+
+    def __call__(self, df):
+        return self.fit_transform(df)
+
+    def fit_transform(self, df):
+        super().fit_transform(df)
+        for col in self.inp:
+            df = pd.concat([df, pd.get_dummies(df[col], prefix=col)], axis=1)
+            df.drop(columns=[col], inplace=True)
+        return df
+
+    def transform(self, df):
+        for col in self.inp:
+            df = pd.concat([df, pd.get_dummies(df[col], prefix=col)], axis=1)
+            df.drop(columns=[col], inplace=True)
         return df
 
 
 # operation types: unary, binary, n-ary
+# here wrong stuff happens: we initialize all classes and all inner variables are always the same
+# TODO: fix this
 OPERATIONS = {
-    "add": Add(),
-    "sub": Sub(),
-    "mul": Mul(),
-    "div": Div(),
-    "pca": Pca(),
-    "fillnamean": FillnaMean(),
-    "fillnamedian": FillnaMedian(),
-    "std": Std(),
-    "minmax": Minmax(),
-    "drop": Drop(),
+    "add": Add,
+    "sub": Sub,
+    "mul": Mul,
+    "div": Div,
+    "pca": Pca,
+    "fillnamean": FillnaMean,
+    "fillnamedian": FillnaMedian,
+    "std": Std,
+    "minmax": Minmax,
+    "drop": Drop,
+    "frequencyencoding": FrequencyEncoding,
+    "binning": Binning,
+    "labelencoding": LabelEncoding,
+    "onehotencoding": OneHotEncoding,
 }
--- src/optimization/data_operations/operation_pipeline.py
+++ src/optimization/data_operations/operation_pipeline.py
@@ -33,16 +33,44 @@ def apply_pipeline(df, parsed_data_operation_pipeline):
     return df
 
 
+class OperationPipeline:
+    def __init__(self):
+        self.operations = []
+
+    def add_operation(self, operation, inp=None):
+        self.operations.append(operation(inp))
+
+    def fit_transform(self, df):
+        for operation in self.operations:
+            df = operation.fit_transform(df)
+        return df
+
+    def transform(self, df):
+        for operation in self.operations:
+            df = operation.transform(df)
+        return df
+
+
 if __name__ == "__main__":
-    data_operation_pipeline = "pca()"
+    data_operation_pipeline = "pca(a)"
     parsed_data_operation_pipeline = parse_data_operation_pipeline(
         data_operation_pipeline
     )
-    test_data = pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [1, 2, 3, 4, 5]})
+    test_data = pd.DataFrame({"a": [5, 2, 2, 4, 5], "b": [1, 2, None, 4, 5]})
     # for operation in parsed_data_operation_pipeline:
     #     function = OPERATIONS[operation]
     #     test_data[operation] = function(test_data,
     #                                     parsed_data_operation_pipeline[
     #                                         operation])
-    test_data = apply_pipeline(test_data, parsed_data_operation_pipeline)
+    # test_data = apply_pipeline(test_data, parsed_data_operation_pipeline)
+    operation_pipeline = OperationPipeline()
+    operation_pipeline.add_operation(OPERATIONS["fillnamean"], "b")
+    operation_pipeline.add_operation(OPERATIONS["pca"], "b")
+    operation_pipeline.add_operation(OPERATIONS["drop"], "a")
+    operation_pipeline.add_operation(OPERATIONS["std"], "b")
+    operation_pipeline.add_operation(OPERATIONS["minmax"], "pca_0")
+    operation_pipeline.add_operation(OPERATIONS["onehotencoding"], "pca_0")
+    test_data = operation_pipeline.fit_transform(test_data)
+
     print(test_data)
+    # print(operation_pipeline.transform(pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [1, 2, 3, 4, 5]})))
