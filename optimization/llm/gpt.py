4c6c5d40a839d3aa035337a13be785975faf5899
AngrySigma <illariov1809@gmail.com>
2024-04-01 21:55:53 +0300

    Remove deprecated MessageHistory class

--- optimization/llm/gpt.py
+++ optimization/llm/gpt.py
@@ -12,32 +12,6 @@ class ChatMessage:
         return self.content
 
 
-class MessageHistory:
-    def __init__(self, messages: list[ChatMessage] | None = None):
-        self.messages = messages if messages is not None else []
-
-    def add_message(self, content: str, role: str = "user"):
-        self.messages.append(ChatMessage(role=role, content=content))
-
-    def add_pipeline_evaluation(self, pipeline: str, metrics: float):
-        self[-2].content += f"\nPipeline: {pipeline}, Metrics: {metrics}"
-
-    def __str__(self):
-        return "\n".join([str(message) for message in self.messages])
-
-    def __add__(self, other):
-        return MessageHistory(self.messages + other.messages)
-
-    def __len__(self):
-        return len(self.messages)
-
-    def __getitem__(self, item):
-        return self.messages[item]
-
-    def __iter__(self):
-        return iter(self.messages)
-
-
 class ChatBot:
     def __init__(self, api_key, api_organization, model="gpt-3.5-turbo"):
         self.client = OpenAI(api_key=api_key, organization=api_organization)
